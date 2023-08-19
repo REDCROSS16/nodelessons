feccde74bcdcabf08d30367791e7e3095dbc0afd
alexredcross <belkill@mail.ru>
2023-08-19 21:32:09 +0300

    update

--- code_01/index.js
+++ code_01/index.js
@@ -1,9 +1,3 @@
 const m1 = require('./example_module');
 
 console.log(m1);
-
-
-    // private const SHIELD_OPEN_SIDE_COMPARISON_KEYS = [
-    //     self::SHIELD_OPEN_SIDE_LEFT => 'e887eacf-ad1f-11ed-80f6-ac1f6b47e527',
-    //     self::SHIELD_OPEN_SIDE_RIGHT =>'f1c95759-ad1f-11ed-80f6-ac1f6b47e527',
-    // ];
