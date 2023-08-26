a9b72145bf4c22b278efb5e6a976bee6c8a7c574
alexredcross <belkill@mail.ru>
2023-08-26 20:16:36 +0300

    update

---
+++ project/index.js
@@ -0,0 +1,9 @@
+const {randomInt, randomArray} = require('./random');
+
+
+
+// for (i = 0; i < 100; i++) {
+    // console.log(randomInt(0,100));
+// }
+
+console.log(randomArray(20));
---
+++ project/random/index.js
@@ -0,0 +1,8 @@
+const rand = require('./random_int')
+const randArray = require('./random_array')
+
+
+module.exports = {
+    'randomInt': rand,
+    'randomArray': randArray
+}
