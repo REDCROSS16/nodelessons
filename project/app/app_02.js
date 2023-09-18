3720e190361af1df3a884a0506aff8339a7c9e47
alexredcross <belkill@mail.ru>
2023-09-18 23:54:46 +0300

    update

--- project/app/app_02.js
+++ project/app/app_02.js
@@ -6,13 +6,10 @@ const sum = require('./module/sum')
 const text = 'abcd\r 1234\r dadasd'
 const pathToFile = 'new.txt';
 
-// write to file
+// write to file запись в файл асинхронно
 // fs.writeFileSync(pathToFile, text, {encoding: 'utf8', flag: 'a'});
 
 // write file from array
 let array = ['new', 'book', 'of', 'bobba', 'fett'];
 
 // fs.writeFileSync(pathToFile, array.join('\r'), {encoding:'utf8', flag:'w'})
-
-
-console.log(sum('sum.txt'));
