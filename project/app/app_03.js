cadddf15c4c22702f79bba2e308e549b2b2e9811
alexredcross <belkill@mail.ru>
2023-09-21 23:41:57 +0300

    update

--- project/app/app_03.js
+++ project/app/app_03.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 
-const pathToFile = 'file.json';
+// const pathToFile = 'file.json';
 //read json
-let json = fs.readFileSync(pathToFile);
-let data = JSON.parse(json)
+// let json = fs.readFileSync(pathToFile);
+// let data = JSON.parse(json)
 
-console.log(data.age);
+// console.log(data.age);
+
+
+const object = {
+    "name" : "Alexander",
+    "surname" : "Podolnitsky",
+    "skill": ["php", "js"]
+}
+
+fs.writeFileSync('test.json', JSON.stringify(object), {encoding: 'utf8', flag: 'w'});
