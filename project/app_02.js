cdfed619cab50847b92bde62578a0353a13bb333
alexredcross <belkill@mail.ru>
2023-10-01 23:47:07 +0300

    update

--- project/app_02.js
+++ project/app_02.js
@@ -2,8 +2,10 @@ const { log } = require('console');
 const http = require('http');
 
 http.createServer(function(req, res) {
-    log(req.url);
-    log(req.method);
+    
+
+
+    
 
     
 }).listen(3500);
