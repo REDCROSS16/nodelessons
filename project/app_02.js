314b93e243a7674535e2d1ec55368c1e98be5924
alexredcross <belkill@mail.ru>
2023-10-03 23:55:04 +0300

    update

--- project/app_02.js
+++ project/app_02.js
@@ -3,7 +3,8 @@ const http = require('http');
 
 http.createServer(function(req, res) {
     res.setHeader('Content-type', "text-html; charset=utf-8;");
-
+    res.write('<h2> hello world</h2>');
+    res.end();
 
     
 
