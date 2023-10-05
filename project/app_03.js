ed9cb691642cb5cb27e5972b8f8db9c4419f3c02
alexredcross <belkill@mail.ru>
2023-10-05 23:53:56 +0300

    update

--- project/app_03.js
+++ project/app_03.js
@@ -6,5 +6,11 @@ http.createServer(function(req, res) {
     const url = req.url;
     log(url);
 
+    switch (url) {
+        case '/':
+            log('Main page');
+            res.write('<h1>Main page</h1>')
+    }
+
 
 }).listen(PORT)
