5c9ec29b337268c0d574b2ce124d043454be7682
alexredcross <belkill@mail.ru>
2023-10-08 19:20:24 +0300

    update

---
+++ project/remove/app_05/app.js
@@ -0,0 +1,29 @@
+const { log } = require('console');
+const http = require('http');
+const fs = require('fs');
+const PORT = 3500;
+
+http.createServer(function(req, res) {
+    const url = req.url;
+    log(url);
+    res.setHeader('Content-type', "text-html; charset=utf-8;");
+
+    switch (url) {
+        case '/':
+            log('Main page');
+            res.write('<h1>Main page</h1>');
+            break;
+        case '/contacts':
+            log('contact page');
+            let data = fs.readFileSync('./contact.html', {encoding: 'utf-8', flag: 'r'});
+            res.write(data);
+            break;
+        default:
+            log('default page');
+            res.write('page 404');        
+    }
+
+    res.end();
+
+
+}).listen(PORT)
